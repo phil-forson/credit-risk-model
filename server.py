6952460a739d06ed478315e258a349f32dd091ec
Philemon Forson <forsonphilemon1@gmail.com>
2026-06-02 12:42:12 -0500

    minor changes

--- server.py
+++ server.py
@@ -113,4 +113,4 @@ def predict():
 
 if __name__ == '__main__':
     # Run the server on port 5000
-    app.run(host='0.0.0.0', port=5000, debug=True)
+    app.run(host='0.0.0.0', port=5001, debug=True)
